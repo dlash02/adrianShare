293bbb04397bfa32b44b6cea500da51ce236d399
adrian-9122 <adriang9122@gmail.com>
2023-11-01 16:00:09 -0500

    Testing the submit
    commiting to main

---
+++ cards.js
@@ -0,0 +1,46 @@
+let deck = {
+    cards : [
+        { 'img' : '2C.png', 'value': 2, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '2D.png', 'value': 2, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '2H.png', 'value': 2, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '2S.png', 'value': 2, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '3C.png', 'value': 3, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '3D.png', 'value': 3, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '3H.png', 'value': 3, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '3S.png', 'value': 3, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '4C.png', 'value': 4, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '4D.png', 'value': 4, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '4H.png', 'value': 4, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '4S.png', 'value': 4, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '5C.png', 'value': 5, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '5D.png', 'value': 5, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '5H.png', 'value': 5, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '5S.png', 'value': 5, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '6C.png', 'value': 6, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '6D.png', 'value': 6, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '6H.png', 'value': 6, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '6S.png', 'value': 6, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '7C.png', 'value': 7, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '7D.png', 'value': 7, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '7H.png', 'value': 7, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '7S.png', 'value': 7, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '8C.png', 'value': 8, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '8D.png', 'value': 8, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '8H.png', 'value': 8, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '8S.png', 'value': 8, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '9C.png', 'value': 9, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '9D.png', 'value': 9, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '9H.png', 'value': 9, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '9S.png', 'value': 9, 'suit' : 'spades', 'dealt': false}
+    ]
+}
+function startGame() {
+    alert( "Starting games")
+}
